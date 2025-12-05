984719a32cc4bd3c417f3c2317988774b4097e84
wotnamed <sjogrenalvin@icloud.com>
2025-12-05 11:07:56 +0100

    book

---
+++ source/src/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    protected String author;
+    protected String year;
+    protected String language;
+    protected String mediaType;
+    protected String category;
+    protected String ISBN;
+    protected String title;
+    protected String originalTitle;
+    protected String length;
+}
