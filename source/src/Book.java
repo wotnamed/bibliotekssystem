62bdc04469653f32f93ff350c94b9e67f82ba7ec
John Doe <johndoe@example.com>
2025-12-12 11:24:52 +0100

    data-loader almost done

--- source/src/Book.java
+++ source/src/Book.java
@@ -2,10 +2,18 @@ public class Book {
     protected String author;
     protected String year;
     protected String language;
-    protected String mediaType;
-    protected String category;
     protected String ISBN;
     protected String title;
-    protected String originalTitle;
     protected String length;
+
+    public Book(String title, String author, String length, String language, String year, String ISBN){
+        this.title = title;
+        this.author = author;
+        this.length = length;
+        this.language = language;
+        this.year = year;
+        this.ISBN = ISBN;
+
+    }
 }
+
