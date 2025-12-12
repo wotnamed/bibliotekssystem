6b322f40af13a4fb79699ee8a19d66d5a9184536
wotnamed <sjogrenalvin@icloud.com>
2025-12-12 14:56:19 +0100

    Authenticator now working

--- source/src/Authenticator.java
+++ source/src/Authenticator.java
@@ -1,21 +1,21 @@
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Authenticator {
     private ArrayList<User> userlist = new ArrayList<User>();
 
     public Authenticator(){
-        this.userlist.add(new User("temp", "1234", new ArrayList<String>()));
+        this.userlist.add(new User("f", "f", new ArrayList<String>()));
         System.out.println(userlist);
     }
 
     public Boolean authCheck(String username, String password){
         boolean status = false;
         for (int i = 0; i < this.userlist.size(); i++) {
-            if (this.userlist.get(i).getUsername() == username && this.userlist.get(i).getPassword() == password){
-                System.out.println("element checked");
+            User user = this.userlist.get(i);
+            // objects.equals seems to work, "==" operator does not work.
+            if (Objects.equals(username, user.getUsername()) && Objects.equals(user.getPassword(), password)){
                 status = true;
-            }else {
-                System.out.println("element checked /fail");
             }
         }
         return status;
