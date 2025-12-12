47ed13b7b57ae7bb26b4727051d4df657818ac70
John Doe <johndoe@example.com>
2025-12-12 10:13:19 +0100

    Book done

--- source/src/Book.java
+++ source/src/Book.java
@@ -2,10 +2,41 @@ public class Book {
     protected String author;
     protected String year;
     protected String language;
-    protected String mediaType;
-    protected String category;
     protected String ISBN;
     protected String title;
-    protected String originalTitle;
-    protected String length;
+    protected String pages;
+
+    Book(String author, String year, String language, String ISBN, String title, String pages){
+        this.author = author;
+        this.year = year;
+        this.language = language;
+        this.ISBN = ISBN;
+        this.title = title;
+        this.pages = pages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPages() {
+        return pages;
+    }
+
+    public String getYear() {
+        return year;
+    }
 }
+
