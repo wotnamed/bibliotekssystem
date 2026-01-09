61ed4c12b1cf0d9b5b6831e100c0f3c741d1e62d
wotnamed <sjogrenalvin@icloud.com>
2026-01-09 09:59:18 +0100

    renamed view to main

--- source/src/Main.java
+++ source/src/Main.java
@@ -1,15 +1,112 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome to another episode of my facts and glitches series!");
+import javax.swing.*;
+import java.awt.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Main().createLoginView());
+    }
+
+    private void createLoginView() {
+        JFrame frame = new JFrame("Login Window");
+        frame.setSize(600, 1000);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        panel.add(Box.createVerticalGlue());
+
+        JTextField usernameField = new JTextField(20);
+        JPasswordField passwordField = new JPasswordField(20);
+        JButton loginButton = new JButton("Log In");
+
+        usernameField.setMaximumSize(new Dimension(200, 30));
+        passwordField.setMaximumSize(new Dimension(200, 30));
+
+        panel.add(centerComponent(new JLabel("Username:")));
+        panel.add(centerComponent(usernameField));
+        panel.add(Box.createRigidArea(new Dimension(0, 20)));
+
+        panel.add(centerComponent(new JLabel("Password:")));
+        panel.add(centerComponent(passwordField));
+        panel.add(Box.createRigidArea(new Dimension(0, 30)));
+
+        panel.add(centerComponent(loginButton));
+
+        panel.add(Box.createVerticalGlue());
+
+        frame.add(panel);
+        frame.setVisible(true);
+
+        // load authentication
+        Authenticator auth = new Authenticator();
+
+        loginButton.addActionListener(e -> {
+            System.out.println("User: "+usernameField.getText());
+            System.out.println("Very-secure password: "+passwordField.getText());
+
+            if (auth.authCheck(usernameField.getText(), passwordField.getText())) {
+                frame.getContentPane().removeAll();
+                createSearchView(frame);
+                frame.revalidate();
+                frame.repaint();
+            } else {
+                System.out.println("Invalid username or password!");
+            }
+        });
+    }
+
+    private void createSearchView(JFrame frame) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        // Top search bar
+        JPanel topPanel = new JPanel();
+        JTextField searchField = new JTextField(20);
+        JButton searchButton = new JButton("Search");
+        topPanel.add(searchField);
+        topPanel.add(searchButton);
+
+        // Center scrollable area for the 3 boxes
+        JPanel boxesContainer = new JPanel();
+        boxesContainer.setLayout(new BoxLayout(boxesContainer, BoxLayout.Y_AXIS));
+
+        // Add three titled boxes
+        boxesContainer.add(createSampleBox("Results Box 1"));
+        boxesContainer.add(Box.createRigidArea(new Dimension(0, 15)));
+        boxesContainer.add(createSampleBox("Results Box 2"));
+        boxesContainer.add(Box.createRigidArea(new Dimension(0, 15)));
+        boxesContainer.add(createSampleBox("Results Box 3"));
+
+        JScrollPane scrollPane = new JScrollPane(boxesContainer);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        panel.add(topPanel, BorderLayout.NORTH);
+        panel.add(scrollPane, BorderLayout.CENTER);
+
+        frame.add(panel);
+    }
+
+    private JPanel createSampleBox(String title) {
+        JPanel boxPanel = new JPanel();
+        boxPanel.setLayout(new BoxLayout(boxPanel, BoxLayout.Y_AXIS));
+        boxPanel.setBorder(BorderFactory.createTitledBorder(title));
+
+        boxPanel.add(new JLabel("Sample Line 1 Sample Line 1 Sample Line 1 Sample Line 1" ));
+        boxPanel.add(new JLabel("Sample Line 2"));
+        boxPanel.add(new JLabel("Sample Line 3"));
+        boxPanel.add(new JLabel("Sample Line 4"));
+        boxPanel.add(new JLabel("Sample Line 5"));
+
+        return boxPanel;
+    }
+
+    // Utility to horizontally center components
+    private Component centerComponent(Component comp) {
+        JPanel wrapper = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        wrapper.add(comp);
+        return wrapper;
     }
 }
